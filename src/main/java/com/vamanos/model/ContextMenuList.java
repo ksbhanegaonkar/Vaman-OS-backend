e9574008d93e4a71d98e83207ec521ea0b756e07
Kedar Bhanegaonkar <ksbhanegaonkar@gmail.com>
2019-08-11 22:37:15 +0530

    Added complete menu list and sent in REST response

--- src/main/java/com/vamanos/model/ContextMenuList.java
+++ src/main/java/com/vamanos/model/ContextMenuList.java
@@ -15,37 +15,68 @@ public class ContextMenuList {
 	//Map<String,List<String>> allMenuList = new HashMap<String, List<String>>();
 	Map<String,List<String>> allMenuList = new HashMap<String, List<String>>();
 	
+	/*
+	 *     "desktop-wallpaper":['','','','','Cut',''],
+      "start-menu-button":['Option 1','Option 2','Option 3','Option 4'],
+      "task-bar":['','Option 6','Option 7','Option 8'],
+      "desktop-item-folder":['','w','','','',''],
+      "desktop-item-file":['','',','','','']
+      ['','','','']
+	 * */
+	
 	public ContextMenuList() {
 		// TODO Auto-generated constructor stub
 		List<String> desktopWallpaperMenuList = new ArrayList<>();
-		desktopWallpaperMenuList.add("menu1");
-		desktopWallpaperMenuList.add("menu2");
-		desktopWallpaperMenuList.add("menu3");
-		desktopWallpaperMenuList.add("menu4");
-		desktopWallpaperMenuList.add("menu5");
+		desktopWallpaperMenuList.add("New Sprint");
+		desktopWallpaperMenuList.add("New User Story");
+		desktopWallpaperMenuList.add("Refresh");
+		desktopWallpaperMenuList.add("Copy");
+		desktopWallpaperMenuList.add("Paste");
+		
+		List<String> taskBarMenuList = new ArrayList<>();
+		taskBarMenuList.add("Task bar option 1");
+		taskBarMenuList.add("Task bar option 2");
+		taskBarMenuList.add("Task bar option 3");
+		taskBarMenuList.add("Task bar option 4");
+		taskBarMenuList.add("Task bar option 5");
+		
+		List<String> desktopItemFolderMenuList = new ArrayList<>();
+		desktopItemFolderMenuList.add("Open folder");
+		desktopItemFolderMenuList.add("Open folder in new window");
+		desktopItemFolderMenuList.add("Bookmark folder");
+		desktopItemFolderMenuList.add("Copy Folder");
+		desktopItemFolderMenuList.add("Rename Folder");
+		desktopItemFolderMenuList.add("Delete Folder");
+		
+
+		
+		List<String> desktopItemFileMenuList = new ArrayList<>();
+		desktopItemFileMenuList.add("Open file");
+		desktopItemFileMenuList.add("Open file in new window");
+		desktopItemFileMenuList.add("Bookmark file");
+		desktopItemFileMenuList.add("Copy file");
+		desktopItemFileMenuList.add("Rename File");
+		desktopItemFileMenuList.add("Delete file");
 		
 		List<String> startButtonContextMenuList = new ArrayList<>();
-		startButtonContextMenuList.add("Start menu1");
-		startButtonContextMenuList.add("Start menu2");
-		startButtonContextMenuList.add("Start menu3");
-		startButtonContextMenuList.add("Start menu4");
-		startButtonContextMenuList.add("Start menu5");
-		startButtonContextMenuList.add("Start menu5");
-		startButtonContextMenuList.add("Start menu5");
-		startButtonContextMenuList.add("Start menu5");
-		startButtonContextMenuList.add("Start menu5");
-		startButtonContextMenuList.add("Start menu5");
-		startButtonContextMenuList.add("Start menu5");
-		startButtonContextMenuList.add("Start menu5");
-		startButtonContextMenuList.add("Start menu5");
-		startButtonContextMenuList.add("Start menu5");
-		startButtonContextMenuList.add("Start menu5");
-		startButtonContextMenuList.add("Start menu5");
-		startButtonContextMenuList.add("Start menu5");
-		startButtonContextMenuList.add("Start menu5");
-		startButtonContextMenuList.add("Start menu5");
+		startButtonContextMenuList.add("My Folder");
+		startButtonContextMenuList.add("My Bookmarks");
+		startButtonContextMenuList.add("My Notes");
+		startButtonContextMenuList.add("Logout");
+
+		
+		List<String> startButtonMenuList = new ArrayList<>();
+		startButtonMenuList.add("My Folder");
+		startButtonMenuList.add("My Bookmarks");
+		startButtonMenuList.add("My Notes");
+		startButtonMenuList.add("Logout");
+
 		allMenuList.put("desktop-wallpaper", desktopWallpaperMenuList);
-		allMenuList.put("start-button-menu", startButtonContextMenuList);
+		allMenuList.put("task-bar", taskBarMenuList);
+		allMenuList.put("desktop-item-folder", desktopItemFolderMenuList);
+		allMenuList.put("desktop-item-file", desktopItemFileMenuList);
+		allMenuList.put("start-button-context-menu", startButtonContextMenuList);
+		allMenuList.put("start-button-menu", startButtonMenuList);
 		
 	}
 
