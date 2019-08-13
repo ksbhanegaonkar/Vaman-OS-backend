a04ea0bb2e3f162fac4b3c006ef215fd8043d1c6
Kedar Bhanegaonkar <ksbhanegaonkar@gmail.com>
2019-08-13 20:22:16 +0530

    Added headers in response filter to fulfill CORS policy. Added post method to update status from frontend

--- src/main/java/com/vamanos/services/ContextMenuService.java
+++ src/main/java/com/vamanos/services/ContextMenuService.java
@@ -1,14 +1,10 @@
 package com.vamanos.services;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
@@ -23,7 +19,18 @@ public class ContextMenuService {
     @Produces(MediaType.APPLICATION_JSON)
     public Response getContextMenuList() {
     	ResponseBuilder builder = Response.ok(new ContextMenuList().getAllMenuList());
-    	Response response = builder.header("Access-Control-Allow-Origin", "*").build();
+    	Response response = builder.build();
+        return response;
+    }
+    
+    @POST
+    @Path("onaction")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.TEXT_PLAIN)
+    public Response onAction() {
+    	System.out.println("On Action method called  !!");
+    	ResponseBuilder builder = Response.ok(new ContextMenuList().getAllMenuList());
+    	Response response = builder.build();
         return response;
     }
 }
